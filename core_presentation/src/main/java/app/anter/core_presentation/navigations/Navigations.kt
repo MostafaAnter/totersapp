1c67b1587278cb6bf063c0ab31682af29506342e
MostafaAnter <ga30000@gmail.com>
2022-12-07 15:23:15 +0200

    setup navigation helper class at core presentation

---
+++ core_presentation/src/main/java/app/anter/core_presentation/navigations/Navigations.kt
@@ -0,0 +1,22 @@
+package app.anter.core_presentation.navigations
+
+import android.content.Context
+import android.content.Intent
+import javax.inject.Inject
+import javax.inject.Singleton
+
+/**
+ * Created by Mostafa Anter.
+ */
+@Singleton
+class Navigations @Inject constructor() {
+    private fun internalIntent(context: Context, action: String) = Intent(action).setPackage(context.packageName)
+
+    //open main screen feature
+    fun openGameListScreenIntent(context: Context) = internalIntent(context, "app.anter.feature_characters_list_screen.open")
+
+    //open detail screen feature
+    fun openGameDetailScreenIntent(context: Context, game_id: Int) = internalIntent(context, "app.anter.feature_detail_screen.open").apply {
+        putExtra("game_id", game_id)
+    }
+}
