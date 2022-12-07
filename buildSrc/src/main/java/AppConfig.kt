587836db19aa7a83a459cab9d77c0155172b1a59
MostafaAnter <ga30000@gmail.com>
2022-12-07 14:30:09 +0200

    setup App Dependencies

---
+++ buildSrc/src/main/java/AppConfig.kt
@@ -0,0 +1,10 @@
+/**
+ * Created by Mostafa Anter on 11/10/20.
+ */
+object AppConfig {
+    const val compileSdk = 33
+    const val minSdk = 21
+    const val targetSdk = 30
+    const val versionCode = 1
+    const val versionName = "1.0.0"
+}
---
+++ buildSrc/src/main/java/Versions.kt
@@ -0,0 +1,72 @@
+/**
+ * Created by Mostafa Anter on 11/10/20.
+ */
+object Versions {
+    //app level
+    const val gradle = "7.0.4"
+    const val kotlin = "1.5.21"
+    const val google_services_version = "4.3.8"
+    const val crashlytics_version = "2.4.1"
+
+    //static code analysis
+    const val detekt_version = "1.14.2"
+    const val detekt_ID = "io.gitlab.arturbosch.detekt"
+
+    //libs
+    const val coreKtx = "1.9.0"
+    const val appcompat = "1.2.0"
+    const val constraintLayout = "2.0.4"
+    const val material = "1.3.0"
+    const val test_truth_version = "1.0.1"
+    const val navigation_fragment = "2.5.3"
+    const val navigation_ui = "2.3.5"
+    const val paging_version = "3.1.1"
+
+    //test
+    const val junit = "4.13.1"
+    const val extJunit = "1.1.2"
+    const val espresso = "3.3.0"
+    const val coroutine_test_version = "1.5.0"
+    const val test_runner_version = "1.3.0"
+    const val test_rules_version = "1.3.0"
+    const val test_core_version = "1.3.0"
+    const val test_ext_version = "1.1.2"
+    const val test_arch_core_version = "2.1.0"
+    const val app_center_test_plugin_version = "1.4"
+
+    //for dagger hilt di
+    const val dagger_hilt_version = "2.44"
+
+    //for network calls
+    const val okHttpVersion = "4.9.1"
+    const val retrofit_version = "2.9.0"
+    const val coroutines_version = "1.4.1"
+    const val moshi_version = "1.14.0"
+
+    //legacy
+    const val legacy_version = "1.0.0"
+
+    //annotation
+    const val annotation_version = "1.1.0"
+
+    //view pager
+    const val viewpager_version = "1.0.0"
+
+    //card view
+    const val card_view_version = "1.0.0"
+
+    //coil version
+    const val coil_version = "1.1.0"
+
+    //firebase version
+    const val firebase_bom_version = "28.1.0"
+
+    //ktx dependencies for lifecycleScope
+    const val scopes_version = "2.5.1"
+
+    //room database
+    const val room_version = "2.3.0-beta03"
+
+    //leak canary for detect memory leak
+    const val leak_canary_version = "2.6"
+}
