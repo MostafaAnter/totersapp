d8888f464535b409ba7cf05f206ef2019dac72b2
MostafaAnter <ga30000@gmail.com>
2022-12-07 14:56:40 +0200

    setup app module and make it act as entry point so in the future we could use it as splash for our app

---
+++ app/src/main/java/app/anter/totersapp/App.kt
@@ -0,0 +1,10 @@
+package app.anter.totersapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * Created by Mostafa Anter on 1/17/22.
+ */
+@HiltAndroidApp
+class App: Application()
--- app/src/main/java/app/anter/totersapp/MainActivity.kt
+++ app/src/main/java/app/anter/totersapp/MainActivity.kt
@@ -1,44 +1,26 @@
 package app.anter.totersapp
 
-import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
-import app.anter.totersapp.databinding.ActivityMainBinding
+import android.os.Bundle
+import app.anter.core_presentation.navigations.Navigations
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    // inject actions to navigate between  modules
+    @Inject
+    lateinit var navigations: Navigations
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        setSupportActionBar(binding.toolbar)
-
-
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
-        }
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
+        // navigate to news search feature
+        startActivity(
+            navigations.openGameListScreenIntent(
+                this
+            )
+        )
+        finish()
     }
 }
