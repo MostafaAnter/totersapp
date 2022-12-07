077defc5c42afbdc2fb96305fea320496d65b1e3
MostafaAnter <ga30000@gmail.com>
2022-12-07 14:42:31 +0200

    convert gradle files to dsl files and setup dependencies for each module

---
+++ app/build.gradle.kts
@@ -0,0 +1,157 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+
+    //for data binding and dagger and more
+    id("kotlin-kapt")
+
+    //for hilt
+    id("com.google.dagger.hilt.android")
+
+    //for google services
+    id("com.google.gms.google-services")
+
+    //for crashlytics
+    id("com.google.firebase.crashlytics")
+}
+
+// to apply detekt code analysis on app module
+apply { from(rootProject.file("detekt.gradle")) }
+
+android {
+    compileSdk = AppConfig.compileSdk
+
+    defaultConfig {
+        applicationId = "app.anter.marvelcharacters"
+        minSdk = AppConfig.minSdk
+        targetSdk = AppConfig.targetSdk
+        versionCode = AppConfig.versionCode
+        versionName = AppConfig.versionName
+        // region multi dex support
+        multiDexEnabled = true
+        // endregion
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        //signingConfig = signingConfigs.getByName("config")
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles (getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+        getByName("debug") {
+            isMinifyEnabled = false
+            proguardFiles (getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+
+    }
+    //support data binding
+    buildFeatures{
+        dataBinding = true
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    // region enable multi dex
+    implementation ("com.android.support:multidex:1.0.3")
+    //endregion
+
+    //for hilt app must depend on all modules
+    //depend on features modules
+    implementation(project(":core"))
+    implementation(project(":core_data"))
+    implementation(project(":core_domain"))
+    implementation(project(":core_presentation"))
+    implementation(project(":feature_characters_list"))
+    implementation(project(":feature_character_detail"))
+
+
+    //region lib
+    implementation (fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+    implementation (AppDependencies.kotlin_stdlib)
+    implementation (AppDependencies.core_ktx)
+    implementation (AppDependencies.androidx_appcompat)
+    implementation (AppDependencies.google_material)
+    implementation (AppDependencies.constraint_layout)
+    implementation (AppDependencies.navigation_fragment)
+    implementation (AppDependencies.navigation_ui)
+    //endregion
+
+    // region dagger
+    implementation (AppDependencies.dagger_hilt)
+    kapt (AppDependencies.dagger_kapt)
+    // end region
+
+    //region firebase
+    implementation(platform(AppDependencies.firebase_bom))
+    implementation (AppDependencies.firebase_analytics)
+    implementation (AppDependencies.firebase_crashlytics)
+    implementation (AppDependencies.firebase_messaging)
+    //endregion
+
+    //region database
+    implementation(AppDependencies.room_database)
+    implementation(AppDependencies.room_database_ktx)
+    kapt(AppDependencies.room_database_kapt)
+    //endregion
+
+    //region leak canary
+    debugImplementation(AppDependencies.leak_canary)
+    //endregion
+
+    // region retrofit network calls
+    implementation (AppDependencies.retrofit)
+    implementation (AppDependencies.retrofit_converter)
+    // for support android 4
+    implementation (AppDependencies.okHttp_interceptor)
+    implementation (AppDependencies.okHttp){
+        version{
+            strictly(Versions.okHttpVersion)
+        }
+    }
+    // endregion
+
+    // region Moshi very important to generate adapters converters
+    implementation (AppDependencies.moshi)
+    kapt(AppDependencies.moshi_kapt)
+    // endregion
+
+    // region Coroutine
+    implementation (AppDependencies.coroutine_android)
+    implementation (AppDependencies.coroutine_core)
+    //endregion
+
+    // coil image loader
+    implementation(AppDependencies.coil)
+
+    // paging lib
+    implementation(AppDependencies.paging_lib)
+
+
+    //region unit test and ui test
+    // JUNIT
+    testImplementation (AppDependencies.junit)
+    androidTestImplementation (AppDependencies.junit_test)
+
+    // ui test, instrumentation test
+    // Espresso
+    androidTestImplementation (AppDependencies.espresso_core)
+    // androidx.test
+    androidTestImplementation (AppDependencies.test_runner)
+    androidTestImplementation (AppDependencies.test_rules)
+    androidTestImplementation (AppDependencies.test_core)
+    androidTestImplementation (AppDependencies.test_ext_junit)
+    //endregion
+}
+
+// Allow references to generated code
+kapt {
+    correctErrorTypes = true
+}
